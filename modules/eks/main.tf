5e7f3a415bc19aa1f43330b625280905b749e18b
yamenmehdizade <yamenmehdizade@gmail.com>
2026-06-05 15:19:31 +0400

    feat: add gp3 storageclass and ebs csi driver

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -114,9 +114,17 @@ resource "aws_iam_openid_connect_provider" "eks" {
 }
 
 
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name             = aws_eks_cluster.this.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.40.0-eksbuild.1"
+  resolve_conflicts_on_create = "OVERWRITE"
+}
 
-
-
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
 
 
 
