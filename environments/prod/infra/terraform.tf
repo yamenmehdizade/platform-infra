94974367b5a3518e06fb28793eb959dd03762af7
yamenmehdizade <yamenmehdizade@gmail.com>
2026-06-12 12:38:15 +0400

    feat: add netdata network monitoring

--- environments/prod/infra/terraform.tf
+++ environments/prod/infra/terraform.tf
@@ -5,10 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    # kubernetes = {
-    #   source  = "hashicorp/kubernetes"
-    #   version = "~> 2.0"
-    # }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -16,16 +16,16 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# data "aws_eks_cluster" "this" {
-#   name = module.eks.cluster_name
-# }
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+}
 
-# data "aws_eks_cluster_auth" "this" {
-#   name = module.eks.cluster_name
-# }
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.this.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.this.token
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
